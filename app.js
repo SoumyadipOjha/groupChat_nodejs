55284194eb198da8e636faebd2e1047f7bb0590a
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-04-06 16:06:27 +0530

    secon commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`));
 
+// nice
+
+
 const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, "public")));
